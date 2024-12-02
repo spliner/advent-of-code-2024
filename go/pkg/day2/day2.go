b26fc89f7efaf4ac87606d220dfb714a58e6ef22
Fillipe Rosini <hi@frosini.dev>
2024-12-02 19:29:31 -0300

    Day 2 in Go

--- go/pkg/day2/day2.go
+++ go/pkg/day2/day2.go
@@ -1,11 +1,103 @@
 package day2
 
-import "bufio"
+import (
+	"bufio"
+	"math"
+	"strconv"
+	"strings"
+)
 
 func Part1(scanner *bufio.Scanner) (string, error) {
-	return "", nil
+	var count int
+	for scanner.Scan() {
+		err := scanner.Err()
+		if err != nil {
+			return "", err
+		}
+
+		line := scanner.Text()
+		rawValues := strings.Split(line, " ")
+		valid, err := isValid(rawValues)
+		if err != nil {
+			return "", err
+		}
+
+		if valid {
+			count += 1
+		}
+	}
+
+	return strconv.Itoa(count), nil
+}
+
+func isValid(rawValues []string) (bool, error) {
+	var previousValue *int
+	var previousDiff float64
+	for _, rawValue := range rawValues {
+		value, err := strconv.Atoi(rawValue)
+		if err != nil {
+			return false, err
+		}
+
+		if previousValue != nil {
+			diff := float64(value - *previousValue)
+			absDiff := math.Abs(diff)
+			if absDiff <= 0 || absDiff > 3 {
+				return false, nil
+			}
+
+			if previousDiff < 0 && diff > 0 {
+				return false, nil
+			}
+
+			if previousDiff > 0 && diff < 0 {
+				return false, nil
+			}
+
+			previousDiff = diff
+		}
+
+		previousValue = &value
+	}
+
+	return true, nil
 }
 
 func Part2(scanner *bufio.Scanner) (string, error) {
-	return "", nil
+	var count int
+	for scanner.Scan() {
+		err := scanner.Err()
+		if err != nil {
+			return "", err
+		}
+
+		line := scanner.Text()
+		rawValues := strings.Split(line, " ")
+		valid, err := isValid(rawValues)
+		if err != nil {
+			return "", err
+		}
+
+		if valid {
+			count += 1
+			continue
+		}
+
+		for i := range len(rawValues) {
+			newValues := make([]string, len(rawValues))
+			copy(newValues, rawValues)
+			newValues = append(newValues[:i], newValues[i+1:]...)
+			valid, err := isValid(newValues)
+			if err != nil {
+				return "", err
+			}
+
+			if valid {
+				count += 1
+				break
+			}
+		}
+	}
+
+	return strconv.Itoa(count), nil
 }
--- go/pkg/day2/day2_test.go
+++ go/pkg/day2/day2_test.go
@@ -1,1 +1,40 @@
 package day2_test
+
+import (
+	"bufio"
+	"strings"
+	"testing"
+
+	"github.com/spliner/aoc2024/pkg/day2"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPart1(t *testing.T) {
+	input := `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`
+
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	result, err := day2.Part1(scanner)
+
+	require.Nil(t, err)
+	require.Equal(t, "2", result)
+}
+
+func TestPart2(t *testing.T) {
+	input := `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`
+
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	result, err := day2.Part2(scanner)
+
+	require.Nil(t, err)
+	require.Equal(t, "4", result)
+}
