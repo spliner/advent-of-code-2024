3f97bbe993eceaf795fd64949df52dc07af16a0e
Fillipe Rosini <hi@frosini.dev>
2024-12-03 12:05:59 -0300

    Day 3 in Go

--- go/pkg/day3/day3.go
+++ go/pkg/day3/day3.go
@@ -2,12 +2,91 @@ package day3
 
 import (
 	"bufio"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
+var regex *regexp.Regexp
+
+func init() {
+	regex = regexp.MustCompile(`mul\((\d+\,\d+)\)`)
+}
+
 func Part1(scanner *bufio.Scanner) (string, error) {
-	return "", nil
+	input, err := mergeLines(scanner)
+	if err != nil {
+		return "", err
+	}
+
+	sum, err := process(input)
+	if err != nil {
+		return "", err
+	}
+
+	return strconv.Itoa(sum), nil
+}
+
+func mergeLines(scanner *bufio.Scanner) (string, error) {
+	var input string
+	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			return "", err
+		}
+
+		input += strings.TrimSpace(scanner.Text())
+	}
+	return input, nil
+}
+
+func process(input string) (int, error) {
+	var sum int
+	var count int
+	matches := regex.FindAllStringSubmatch(input, -1)
+	for _, m := range matches {
+		split := strings.Split(m[1], ",")
+		left, err := strconv.Atoi(string(split[0]))
+		if err != nil {
+			return 0, err
+		}
+
+		right, err := strconv.Atoi(string(split[1]))
+		if err != nil {
+			return 0, err
+		}
+
+		sum += left * right
+		count += 1
+	}
+
+	return sum, nil
 }
 
 func Part2(scanner *bufio.Scanner) (string, error) {
-	return "", nil
+	input, err := mergeLines(scanner)
+	if err != nil {
+		return "", err
+	}
+
+	split := strings.Split(input, "don't")
+	sum, err := process(split[0])
+	if err != nil {
+		return "", err
+	}
+
+	for _, dont := range split[1:] {
+		_, after, found := strings.Cut(dont, "do()")
+		if !found {
+			continue
+		}
+
+		s, err := process(after)
+		if err != nil {
+			return "", err
+		}
+
+		sum += s
+	}
+
+	return strconv.Itoa(sum), nil
 }
--- go/pkg/day3/day3_test.go
+++ go/pkg/day3/day3_test.go
@@ -1,1 +1,30 @@
 package day3_test
+
+import (
+	"bufio"
+	"strings"
+	"testing"
+
+	"github.com/spliner/aoc2024/pkg/day3"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPart1(t *testing.T) {
+	input := "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	result, err := day3.Part1(scanner)
+
+	require.Nil(t, err)
+	require.Equal(t, "161", result)
+}
+
+func TestPart2(t *testing.T) {
+	input := "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	result, err := day3.Part2(scanner)
+
+	require.Nil(t, err)
+	require.Equal(t, "48", result)
+}
