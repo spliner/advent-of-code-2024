2a4d40817e68fbd105ce475f18656fe4193b9b80
Fillipe Rosini <hi@frosini.dev>
2024-12-09 13:27:49 -0300

    Day 5 in Go

--- go/pkg/day5/day5.go
+++ go/pkg/day5/day5.go
@@ -1,11 +1,123 @@
 package day5
 
-import "bufio"
+import (
+	"bufio"
+	"slices"
+	"strconv"
+	"strings"
+)
 
 func Part1(scanner *bufio.Scanner) (string, error) {
-	return "", nil
+	rules, err := parseRules(scanner)
+	if err != nil {
+		return "", err
+	}
+
+	var sum int
+	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			return "", err
+		}
+
+		line := strings.TrimSpace(scanner.Text())
+		pages := strings.Split(line, ",")
+		valid := isValid(pages, rules)
+
+		if valid {
+			n, err := strconv.Atoi(pages[len(pages)/2])
+			if err != nil {
+				return "", err
+			}
+
+			sum += n
+		}
+	}
+
+	return strconv.Itoa(sum), nil
+}
+
+func parseRules(scanner *bufio.Scanner) (map[string]map[string]struct{}, error) {
+	rules := make(map[string]map[string]struct{})
+	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			return nil, err
+		}
+
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			break
+		}
+
+		before, after, _ := strings.Cut(line, "|")
+		pages, ok := rules[before]
+		if !ok {
+			pages = make(map[string]struct{})
+		}
+		pages[after] = struct{}{}
+		rules[before] = pages
+	}
+
+	return rules, nil
+}
+
+func isValid(pages []string, rules map[string]map[string]struct{}) bool {
+	for i := 1; i < len(pages); i++ {
+		page := pages[i]
+		pageRules, ok := rules[page]
+		if !ok {
+			continue
+		}
+
+		for j := i - 1; j >= 0; j-- {
+			other := pages[j]
+			_, ok := pageRules[other]
+			if ok {
+				return false
+			}
+		}
+	}
+
+	return true
 }
 
 func Part2(scanner *bufio.Scanner) (string, error) {
-	return "", nil
+	rules, err := parseRules(scanner)
+	if err != nil {
+		return "", err
+	}
+
+	var sum int
+	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			return "", err
+		}
+
+		line := strings.TrimSpace(scanner.Text())
+		pages := strings.Split(line, ",")
+		valid := isValid(pages, rules)
+
+		if !valid {
+			slices.SortFunc(pages, func(a, b string) int {
+				pageRules, ok := rules[a]
+				if !ok {
+					return 0
+				}
+
+				_, ok = pageRules[b]
+				if ok {
+					return -1
+				}
+
+				return 1
+			})
+			n, err := strconv.Atoi(pages[len(pages)/2])
+			if err != nil {
+				return "", err
+			}
+
+			sum += n
+		}
+	}
+
+	return strconv.Itoa(sum), nil
 }
